33f88124c08b938663c00bc4da1af4fb32c8238f
Tomasz Bakula <tbakula92@gmail.com>
2017-08-23 18:23:27 +0100

    Initial commit.

---
+++ src/containers/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    render () {
+        return (
+            <form className="input-group">
+                <input
+                    placeholder="Type the city name..."
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+
+    onInputChange (event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
+
+}
